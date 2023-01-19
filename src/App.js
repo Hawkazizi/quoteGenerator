8de1877cae0168cf9da92a4a7212a5815ca082ab
Shahin Azizi <hawk.azizi1@gmail.com>
2023-01-20 01:44:57 +0330

    add some functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+const url = "https://api.quotable.io/random";
 
-function App() {
+const App = () => {
+  const [quotes, setQuotes] = useState([]);
+
+  //Fetch Quotes from API
+  const getQuote = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setQuotes(data));
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getNewQuote = () => {
+    getQuote();
+  };
+
+  const tweetQuote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quotes.content} - ${quotes.author}`;
+    window.open(twitterUrl, "_blank");
+  };
+
+  const { content, author } = quotes;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box-centerside">
+      <div className="text">
+        <p>{content}</p>
+      </div>
+      <div className="author">
+        <h5>{author}</h5>
+        <div className="button-container">
+          <button className="twitter-button" onClick={tweetQuote}>
+            <i className="fab fa-twitter"></i>
+          </button>
+          <button onClick={getNewQuote}>New Quote</button>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
